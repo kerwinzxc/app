5132f291d1b5189cd6a78cbbec6ec010831382df
shaovie <shaovie@gmail.com>
2015-04-11 01:10:24 +0800

    modify

--- tmp/test.php
+++ tmp/test.php
@@ -7,12 +7,14 @@ for ($i = 13810421800; $i <= 13810421899; $i++) {
   $sid = $ret['sid'];
 
   $sex = $i % 2;
-  $ret = json_decode(file_get_contents("http://{$ip}/api/add_patient.php?sid={$sid}&name=123456&phone_num={$i}&sex={$sex}"), true);
+  $ret = json_decode(file_get_contents("http://{$ip}/api/add_user_patient.php?sid={$sid}&name=123456&phone_num={$i}&sex={$sex}"), true);
   $ret = json_decode(file_get_contents("http://{$ip}/api/get_user_patient_list.php?sid={$sid}"), true);
   printf("get %s ", $ret['list']);
   if (count($ret['list']) > 1) {
     $patient_id = $ret['list'][1]['id'];
     $ret = json_decode(file_get_contents("http://{$ip}/api/del_user_patient.php?sid={$sid}&patient_id={$patient_id}"), true);
     printf("del %s\n", $ret['patient_id']);
+  } else {
+    printf("\n");
   }
 }
