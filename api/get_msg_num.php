8858aabf68efdb1625e033ba986a48e00434664b
shaovie <shaovie@gmail.com>
2015-04-12 19:12:21 +0800

    modify

---
+++ api/get_msg_num.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__ . '/../init.php';
+
+if ($_SERVER['REQUEST_METHOD'] != 'GET') exit;
+
+$ret_code = 0;
+$result = array();
+
+do {
+  if (empty($_GET['sid'])) {
+    $ret_code = ERR_PARAM_INVALID;
+    break;
+  }
+  $sid = $_GET['sid'];
+  if (!user_session::is_sid($sid)) {
+    $ret_code = ERR_PARAM_INVALID;
+    break;
+  }
+
+  $s_info = user_session::get_session($sid);
+  if ($s_info === false) {
+    $ret_code = ERR_NOT_LOGIN;
+    break;
+  }
+  $s_info = json_decode($s_info, true);
+  if (empty($s_info)) {
+    $ret_code = ERR_NOT_LOGIN;
+    break;
+  }
+  $user_id = $s_info['user_id'];
+
+  $ret_body['msg_num'] = 1;
+
+} while (false);
+
+$ret_body['code'] = $ret_code;
+$ret_body['desc'] = $ERRORS[$ret_code];
+
+echo json_encode($ret_body);
---
+++ class/fn_doctor.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class fn_doctor
+{
+  public static function build_doctor_detail_list($doctor_list)
+  {
+    $doctor_detail_list = array();
+    foreach ($doctor_list as $doctor_id) {
+      $doctor_info = tb_doctor::query_doctor_by_id($doctor_id);
+      if (empty($doctor_info)) {
+        continue;
+      }
+      $doctor_detail = array();
+      $doctor_detail['id'] = $doctor_info['id'];
+      $doctor_detail['name'] = $doctor_info['name'];
+      $doctor_detail['icon_url'] = $doctor_info['icon_url'];
+      $doctor_detail['title'] = $doctor_info['title'];
+      $doctor_detail['hospital'] = $doctor_info['hospital'];
+      $doctor_detail['expert_in'] = $doctor_info['expert_in'];
+
+      $doctor_detail_list[] = $doctor_detail;
+    }
+    return $doctor_detail_list;
+  }
+};
